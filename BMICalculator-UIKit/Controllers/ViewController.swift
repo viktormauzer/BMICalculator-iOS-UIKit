b9e39b8c6084d70ce565d5a205143a826f582ef8
Viktor Mauzer <mauzer.viktor@icloud.com>
2021-10-04 16:26:28 +0200

    Set up Initial ViewController

--- BMICalculator-UIKit/Controllers/ViewController.swift
+++ BMICalculator-UIKit/Controllers/ViewController.swift
@@ -14,19 +14,44 @@ class ViewController: UIViewController {
     @IBOutlet var heightSlider: UISlider!
     @IBOutlet var weightSlider: UISlider!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        determineDeviceOrientation()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        determineDeviceOrientation()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
     }
+    
+    
+    //MARK: - IBActions
 
     @IBAction func calculateButtonPressed(_ sender: UIButton) {
     }
     
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-
+        heightLabel.text = "\(String(format: "%.1f", sender.value)) centimeters"
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = "\(String(format: "%.1f", sender.value)) kilograms"
+    }
+    
+    
+    //MARK: - Additional Functions
+    
+    func determineDeviceOrientation() {
+        if UIDevice.current.orientation.isLandscape {
+            logoImage.isHidden = true
+        } else {
+            logoImage.isHidden = false
+        }
     }
 }
 
